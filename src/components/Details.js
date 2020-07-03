4dcb0443a6c10fceed1178743cb40040e93082f1
Abhinav Gurung <abhinav.gurung1@gmail.com>
2020-07-03 01:50:41 -0400

    minor css changes

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import {
 	Table,
 	TableBody,
@@ -14,10 +14,30 @@ import {
 		table: {
 			minWidth: 650,
 		},
-		head: {
-			backGroundColor: '#ffab00',
-		}
 	});
+
+	const StyledTableCell = withStyles((theme) => ({
+  head: {
+		background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
 
 
 let detailUrl = 'https://covid19.mathdro.id/api/countries/';
@@ -39,27 +59,27 @@ const Details = ({ country}) => {
 		<div>
 			{country !== 'Global' &&
 				<TableContainer component={Paper}>
-					<Table className={classes.table} size="medium" aria-label="a dense table">
-						<TableHead className={classes.head}>
+					<Table className={classes.table} size="medium" aria-label="customized table">
+						<TableHead>
 							<TableRow>
-								<TableCell align="center">Country</TableCell>
-								<TableCell align="center">Province</TableCell>
-								<TableCell align="center">Confirmed</TableCell>
-								<TableCell align="center">Recovered</TableCell>
-								<TableCell align="center">Deaths</TableCell>
+								<StyledTableCell align="center">Country</StyledTableCell>
+								<StyledTableCell align="center">Province</StyledTableCell>
+								<StyledTableCell align="center">Confirmed</StyledTableCell>
+								<StyledTableCell align="center">Recovered</StyledTableCell>
+								<StyledTableCell align="center">Deaths</StyledTableCell>
 							</TableRow>
 						</TableHead>
 						<TableBody>
 							{list.map((item) => (
-								<TableRow key={item.uid}>
-									<TableCell component="th" scope="row" align="center">
+								<StyledTableRow key={item.uid}>
+									<StyledTableCell component="th" scope="row" align="center">
 										{item.countryRegion}
-									</TableCell>
-									<TableCell align="center">{item.provinceState}</TableCell>
-									<TableCell align="center">{item.confirmed}</TableCell>
-									<TableCell align="center">{item.recovered}</TableCell>
-									<TableCell align="center">{item.deaths}</TableCell>
-								</TableRow>
+									</StyledTableCell>
+									<StyledTableCell align="center">{item.provinceState}</StyledTableCell>
+									<StyledTableCell align="center">{item.confirmed}</StyledTableCell>
+									<StyledTableCell align="center">{item.recovered}</StyledTableCell>
+									<StyledTableCell align="center">{item.deaths}</StyledTableCell>
+								</StyledTableRow>
 							))}
 						</TableBody>
 					</Table>
